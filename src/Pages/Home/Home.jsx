5c5ad8db07cdaa78f2f76bc1b245a5841131f174
CamiloDuranMesa <duranmesacamilo@gmail.com>
2025-03-22 11:53:45 -0500

    Fetch: titulo corregido

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
     <Main >
         <div className="bg-teal-100 h-full w-full flex flex-col justify-center items-center gap-3">
           <ItemTitle styles="text-2xl font-bold text-gray-500 m-xl" content="Bienvenido a mi taller evaluativo"/>
-          <h2 className='text-xl font-bold text-gray-500 m-xl'>En el footer se podran encontrar las redirecciones a la solución de cada ejercicio.</h2>
+          <h2 className='text-xl font-bold text-gray-500 m-xl'>En el header se podran encontrar las redirecciones a la solución de cada ejercicio.</h2>
            </div>
     </Main>
   )
